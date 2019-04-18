5a944e369fa8f2f1349f8f707a5e540f2d92afc5
ydhnwb <yudhanewbie@gmail.com>
2019-04-18 17:37:12 +0700

    fixing updare

--- app/Http/Controllers/Bug/BugController.php
+++ app/Http/Controllers/Bug/BugController.php
@@ -29,7 +29,9 @@ class BugController extends Controller{
 
         $validator = Validator::make($request->all(), $rules);
         if($validator->fails()){
-            return response() -> json(array('message' => 'check your request again. desc must be 10 char or more and form must be filled', 'status' => false), 400);
+            return response() -> json(array(
+                'message' => $request->all(),
+                'status' => false), 400);
         }else{
             $photo = $request->file('photo');
             $extension = $photo->getClientOriginalExtension();
