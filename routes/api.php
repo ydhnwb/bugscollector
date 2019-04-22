44d824b7fab413b40f43f9dc7139640a754d543e
ydhnwb <yudhanewbie@gmail.com>
2019-04-22 19:50:16 +0700

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -23,5 +23,6 @@ Route::post('bugs','Bug\BugController@store');
 Route::get('bugs','Bug\BugController@index');
 Route::post('bugs/{id}','Bug\BugController@update');
 Route::get('bugs/{id}','Bug\BugController@show');
+Route::delete('bugs/{id}', 'Bug\BugController@destroy');
 Route::post('bugs/search/result', 'Bug\BugController@search');
 
